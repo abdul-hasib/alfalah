6e5e63e226887e0c372b0cbaf5fa2c8e24acac0b
Abdul <abdulh@vmware.com>
2016-09-17 16:36:08 +0530

    RESTURI file is added

---
+++ alfalahindia/src/main/java/org/apps/alfalahindia/rest/RestURI.java
@@ -0,0 +1,44 @@
+package org.apps.alfalahindia.rest;
+
+
+import java.util.Map;
+
+public class RestURI {
+
+    private static final String SERVER_BASE_URI = "http://alfalahindia.org/rest";
+
+    private static final String LOCAL_BASE_URI = "http://192.168.184.1/alifrest";
+
+    private static String getBaseURI() {
+        return SERVER_BASE_URI;
+    }
+
+    public static String getUri(String path) {
+        return getBaseURI() + path;
+    }
+
+    public static String getUri(String path, Map<String, String> params) {
+        StringBuilder sb = new StringBuilder(getBaseURI());
+        sb.append(path);
+        int i = 1;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String key;
+            String value;
+            key = entry.getKey();
+            value = entry.getValue();
+            if (i == 1) {
+                sb.append("?").append(key).append("=").append(value);
+            } else {
+                sb.append("&").append(key).append("=").append(value);
+            }
+
+            i++;
+
+        }
+        String url = sb.toString();
+
+        System.out.println(url);
+
+        return url;
+    }
+}
