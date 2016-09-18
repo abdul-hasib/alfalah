96a0acbefeb0902a0c816099bd4cda6f0bcf316c
Abdul <abdulh@vmware.com>
2016-09-18 19:03:42 +0530

    datetime issue is fixed

--- alfalahindia/src/main/java/org/apps/alfalahindia/Managers/DatetimeManager.java
+++ alfalahindia/src/main/java/org/apps/alfalahindia/Managers/DatetimeManager.java
@@ -3,11 +3,15 @@ package org.apps.alfalahindia.Managers;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import org.apps.alfalahindia.Util.DatetimeUtil;
+
 import java.util.Calendar;
+import java.util.Date;
 
 public class DatetimeManager implements View.OnFocusChangeListener, DatePickerDialog.OnDateSetListener {
 
@@ -19,22 +23,37 @@ public class DatetimeManager implements View.OnFocusChangeListener, DatePickerDi
         this.context = context;
         this.editText = editText;
         this.editText.setOnFocusChangeListener(this);
-        this.calendar = Calendar.getInstance();
     }
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
         if (hasFocus) {
+            calendar = Calendar.getInstance();
+
+            String textValue = editText.getText().toString();
+            if (!textValue.isEmpty()) {
+                Date dt = DatetimeUtil.parseDate(textValue);
+                if (dt != null) {
+                    calendar.setTimeInMillis(dt.getTime());
+                }
+            }
+
             int year = calendar.get(Calendar.YEAR);
             int month = calendar.get(Calendar.MONTH);
             int day = calendar.get(Calendar.DAY_OF_MONTH);
+
             new DatePickerDialog(context, this, year, month, day).show();
         }
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        String date = day + "/" + month + "/" + year;
-        this.editText.setText(date);
+        StringBuilder date = new StringBuilder()
+                .append(day).append("/")
+                .append(month + 1).append("/")
+                .append(year);
+
+        Log.d("ALIF", date.toString());
+        this.editText.setText(date.toString());
     }
 }
